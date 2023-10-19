67520482cf347ce13048d1eb871bb49d5858a825
bugagashkovich <bugagashkovich@yandex.ru>
2023-10-19 04:01:00 +0300

    Доработана пагинация

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="container mx-auto mt-4">
     <h1 class="text-bold text-3xl mx-auto my-16 flex justify-center">
-      PRODUCTS, {{ currentChunk }}
+      PRODUCTS
     </h1>
     <div class="grid grid-cols-5 gap-5">
       <div v-for="p in viewData">
         <ProductCard :product="p" />
       </div>
     </div>
-    <div class="flex gap-4 m-12 center">
+    <div class="flex gap-4 m-12 justify-center">
       <div v-for="(c, i) in chunkData" :key="i">
-        <button v-on:click="if (chunkData) viewData = chunkData[i];">
-          {{ i }}
-        </button>
+        <button
+          v-on:click="
+            if (chunkData) {
+              viewData = chunkData[i];
+              currentChunk = i;
+            }
+          "
+          :class="[
+            currentChunk === i
+              ? 'h-3 w-3 bg-[#60a5fa] rounded-sm shadow-sm'
+              : 'h-3 w-3 bg-green-200 rounded-sm shadow-sm',
+          ]"
+        ></button>
       </div>
     </div>
   </div>
